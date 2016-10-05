11cad9d5c3ebeb67fdfe5658de9c3e65c4316422
Jerome <jdc@free.fr>
2016-10-05 16:30:19 +0200

    simplify inputEqual method

--- app/src/main/java/com/jerome/calcul/CalcUL.java
+++ app/src/main/java/com/jerome/calcul/CalcUL.java
@@ -33,6 +33,10 @@ public class CalcUL {
         return display.toString();
     }
 
+    private void setDisplay(double value) {
+        setDisplay(String.valueOf(value).replaceAll("\\.0*$", ""));
+    }
+
     public String manageAction(String myTag) {
         if (myTag.equals("C")) {
             inputClear();
@@ -51,10 +55,6 @@ public class CalcUL {
     private void setDisplay(String value) {
         display.delete(0, display.length());
         display.append(value.replaceAll("\\.0*$", ""));
-    }
-
-    private void setDisplay(double value) {
-        setDisplay(String.valueOf(value).replaceAll("\\.0*$", ""));
     }
 
     private void inputClear() {
@@ -78,6 +78,7 @@ public class CalcUL {
             changePendingOperand(myTag);
         }
     }
+
     private void changePendingOperand(String myTag) {
         stockPendingOperation();
         setOperand2("");
@@ -101,51 +102,77 @@ public class CalcUL {
         setPendingOperation(myTag);
         setPendingOperand(false);
     }
-
-    private void inputEqual() {
-        //setOperation(false);
+    private double getOperand1(){
         double current_operand1;
+        if (operand1.toString().equals("")) {
+            current_operand1 = getOperand2();
+        } else {
+            current_operand1 = Double.valueOf(operand1.toString());
+        }
+        return current_operand1;
+    }
+    private double getOperand2() {
         double current_operand2;
-        double current_result;
         if (operand2.toString().equals("")) {
             current_operand2 = Double.valueOf(getDisplay());
             setOperand2(String.valueOf(current_operand2));
         } else {
             current_operand2 = Double.valueOf(operand2.toString());
         }
-        if (operand1.toString().equals("")) {
-            current_operand1 = current_operand2;
-        } else {
-            current_operand1 = Double.valueOf(operand1.toString());
-        }
+        setOperand2(String.valueOf(current_operand2));
         setPendingOperand(false);
+        return current_operand2;
+    }
+    private void checkPendingOperation() {
         if (!pending_operation.toString().equals("")) {
             setOperation(pending_operation.toString());
             setPendingOperation("");
         }
+    }
+    private void inputEqual() {
+        double current_operand1 = getOperand1();
+        double current_operand2 = getOperand2();
+        double current_result = Double.valueOf(defaultValue);
+        checkPendingOperation();
         if (operation.toString().equals("/")) {
-            if (current_operand2 != 0) {
-                current_result = current_operand1 / current_operand2;
-                //Log.e("my_div_calc", current_result + " = " + current_operand1 + " / " + current_operand2);
-                setDisplay(current_result);
-                setOperand1(getDisplay());
-            } else {
-                ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
-                toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-            }
+            current_result = doDiv(current_operand1, current_operand2);
         } else if (operation.toString().equals("*")) {
-            current_result = current_operand1 * current_operand2;
-            setDisplay(current_result);
-            setOperand1(getDisplay());
+            current_result = doMul(current_operand1, current_operand2);
         } else if (operation.toString().equals("-")) {
-            current_result = current_operand1 - current_operand2;
-            setDisplay(current_result);
-            setOperand1(getDisplay());
+            current_result = doMin(current_operand1, current_operand2);
         } else if (operation.toString().equals("+")) {
-            current_result = current_operand1 + current_operand2;
-            setDisplay(current_result);
-            setOperand1(getDisplay());
+            current_result = doAdd(current_operand1, current_operand2);
         }
+        setDisplay(current_result);
+        setOperand1(getDisplay());
+    }
+
+    private double doDiv(double operand1, double operand2) {
+        double current_result;
+        if (operand2 != 0) {
+            current_result = operand1 / operand2;
+        } else {
+            inputClear();
+            current_result = Double.valueOf(defaultValue);
+            ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+            toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+        }
+        return current_result;
+    }
+
+    private double doMul(double operand1, double operand2) {
+        double current_result = operand1 * operand2;
+        return current_result;
+    }
+
+    private double doMin(double operand1, double operand2) {
+        double current_result = operand1 - operand2;
+        return current_result;
+    }
+
+    private double doAdd(double operand1, double operand2) {
+        double current_result = operand1 + operand2;
+        return current_result;
     }
 
     private void setOperand1(String value) {
@@ -173,16 +200,6 @@ public class CalcUL {
 
     private void setPendingOperand(boolean value) {
         pending_operand = value;
-    }
-
-    private void setOperation(boolean resetDisplay) {
-        if (!pending_operation.toString().equals("")) {
-            setOperation(pending_operation.toString());
-            setPendingOperation("");
-            if (resetDisplay) {
-                setDisplay(defaultValue);
-            }
-        }
     }
 
     private void setOperation(String value) {
