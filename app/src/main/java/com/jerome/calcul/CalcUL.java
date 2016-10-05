ed33f9ad2205d76fe838b87b7b3a8a0470b997db
Jerome <jdc@free.fr>
2016-10-05 11:07:31 +0200

    first working version with tests

--- app/src/main/java/com/jerome/calcul/CalcUL.java
+++ app/src/main/java/com/jerome/calcul/CalcUL.java
@@ -2,6 +2,7 @@ package com.jerome.calcul;
 
 import android.media.AudioManager;
 import android.media.ToneGenerator;
+import android.util.Log;
 
 import java.util.Arrays;
 
@@ -9,93 +10,153 @@ import java.util.Arrays;
  * Created by Human Booster on 04/10/2016.
  */
 public class CalcUL {
-    private static StringBuffer result;
+    private static StringBuffer display;
     private static StringBuffer operand1;
-    private static StringBuffer current_operation;
+    private static StringBuffer operand2;
     private static StringBuffer operation;
-    private String defaultResult = "0";
-    public CalcUL(){
-        if (result == null) {
-            result = new StringBuffer(defaultResult);
-            operand1 = new StringBuffer(defaultResult);
-            current_operation = new StringBuffer("");
-            operation = new StringBuffer("");
+    private static StringBuffer current_operation;
+    private String defaultValue = "0";
+
+    public CalcUL() {
+        if (display == null) {
+            display = new StringBuffer();
+            operand1 = new StringBuffer();
+            operand2 = new StringBuffer();
+            operation = new StringBuffer();
+            current_operation = new StringBuffer();
+            inputClear();
         }
     }
-    public String getResult(){
-        return result.toString();
+
+    public String getDisplay() {
+        return display.toString();
     }
+
+    private void setDisplay(double value) {
+        setDisplay(String.valueOf(value).replaceAll("\\.0*$", ""));
+    }
+
+    public void inputClear() {
+        setDisplay(defaultValue);
+        setOperand1("");
+        setOperand2("");
+        setOperation("");
+        setCurrentOperation("");
+    }
+
+    public void inputDigit(String myTag) {
+        setOperation(true);
+        if (defaultValue.equals(display.toString())) {
+            display.delete(0, display.length());
+        }
+        setOperand2("");
+        display.append(myTag);
+    }
+
+    public void inputDecimalSeparator(String myTag) {
+        if (display.lastIndexOf(myTag) < 0) {
+            setOperation(true);
+            display.append(myTag);
+            setOperand2("");
+        }
+    }
+
+    public void inputOperation(String myTag) {
+        if (!operation.toString().equals("") && !operand1.toString().equals("") && current_operation.toString().equals("")) {
+            inputEqual();
+        }
+        setOperand1(display.toString());
+        setCurrentOperation(myTag);
+    }
+
+    public void inputEqual() {
+        setOperation(false);
+        double current_operand1;
+        double current_operand2;
+        if (operand2.toString().equals("")) {
+            current_operand2 = Double.valueOf(getDisplay());
+            setOperand2(String.valueOf(current_operand2));
+        } else {
+            current_operand2 = Double.valueOf(operand2.toString());
+        }
+        double current_result;
+        if (operand1.toString().equals("")) {
+            current_operand1 = current_operand2;
+        } else {
+            current_operand1 = Double.valueOf(operand1.toString());
+        }
+        if (operation.toString().equals("/")) {
+            if (current_operand2 != 0) {
+                current_result = current_operand1 / current_operand2;
+                Log.e("my_div_calc", current_result + " = " + current_operand1 + " / " + current_operand2);
+                setDisplay(current_result);
+                setOperand1(getDisplay());
+            } else {
+                ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
+                toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+            }
+        } else if (operation.toString().equals("*")) {
+            current_result = current_operand1 * current_operand2;
+            setDisplay(current_result);
+            setOperand1(getDisplay());
+        } else if (operation.toString().equals("-")) {
+            current_result = current_operand1 - current_operand2;
+            setDisplay(current_result);
+            setOperand1(getDisplay());
+        } else if (operation.toString().equals("+")) {
+            current_result = current_operand1 + current_operand2;
+            setDisplay(current_result);
+            setOperand1(getDisplay());
+        }
+    }
+
     public String manageAction(String myTag) {
         if (myTag.equals("C")) {
-            setResult(defaultResult);
-            setOperand1(defaultResult);
-            setOperation("");
-            setCurrentOperation("");
+            inputClear();
         } else if (Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0").contains(myTag)) {
-            setOperation();
-            if (defaultResult.equals(result.toString())) {
-                result.delete(0, result.length());
-            }
-            result.append(myTag);
+            inputDigit(myTag);
         } else if (myTag.equals(".")) {
-            if (result.lastIndexOf(myTag) < 0) {
-                setOperation();
-                result.append(myTag);
-            }
+            inputDecimalSeparator(myTag);
         } else if (Arrays.asList("*", "-", "+", "/").contains(myTag)) {
-            setOperand1(result.toString());
-            setCurrentOperation(myTag);
+            inputOperation(myTag);
         } else if (myTag.equals("=")) {
-            if (!operand1.toString().equals("") && !operation.toString().equals("")) {
-                double current_operand1 = Double.valueOf(operand1.toString());
-                double current_operand2 = Double.valueOf(getResult());
-                double current_Result;
-                if (operation.toString().equals("/")) {
-                    if (current_operand2 != 0) {
-                        current_Result = current_operand1 / current_operand2;
-                        setResult(current_Result);
-                    } else {
-                        ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
-                        toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-                    }
-                } else if (operation.toString().equals("*")) {
-                    current_Result = current_operand1 * current_operand2;
-                    setResult(current_Result);
-                } else if (operation.toString().equals("-")) {
-                    current_Result = current_operand1 - current_operand2;
-                    setResult(current_Result);
-                } else if (operation.toString().equals("+")) {
-                    current_Result = current_operand1 + current_operand2;
-                    setResult(current_Result);
-                }
-            }
+            inputEqual();
         }
-        return getResult();
+        return getDisplay();
     }
-    private void setResult(String value) {
-        result.delete(0, result.length());
-        result.append(value);
+
+    private void setDisplay(String value) {
+        display.delete(0, display.length());
+        display.append(value.replaceAll("\\.0*$", ""));
     }
-    private void setResult(double value) {
-        setResult(String.valueOf(value).replaceAll("[.0]*$", ""));
-    }
+
     private void setOperand1(String value) {
         operand1.delete(0, operand1.length());
         operand1.append(value);
     }
+
+    private void setOperand2(String value) {
+        operand2.delete(0, operand2.length());
+        operand2.append(value);
+    }
+
     private void setCurrentOperation(String value) {
         current_operation.delete(0, current_operation.length());
         current_operation.append(value);
     }
-    private void setOperation() {
+
+    private void setOperation(boolean resetDisplay) {
         if (!current_operation.toString().equals("")) {
             setOperation(current_operation.toString());
             setCurrentOperation("");
-            setResult(defaultResult);
+            if (resetDisplay) {
+                setDisplay(defaultValue);
+            }
         }
     }
+
     private void setOperation(String value) {
         operation.delete(0, operation.length());
         operation.append(value);
     }
- }
+}
--- app/src/test/java/com/jerome/calcul/ExampleUnitTest.java
+++ app/src/test/java/com/jerome/calcul/ExampleUnitTest.java
@@ -14,4 +14,69 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void checkInputDigit() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        assertEquals("1", myCalc.manageAction("1"));
+    }
+    @Test
+    public void checkInputCommaDigit() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction(".");
+        assertEquals("0.1", myCalc.manageAction("1"));
+    }
+    @Test
+    public void checkComputePlus() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("2");
+        myCalc.manageAction("+");
+        myCalc.manageAction("7");
+        assertEquals("9", myCalc.manageAction("="));
+    }
+    @Test
+    public void checkComputeMinus() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("9");
+        myCalc.manageAction("-");
+        myCalc.manageAction("7");
+        assertEquals("2", myCalc.manageAction("="));
+    }
+    @Test
+    public void checkComputeTimes() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("2");
+        myCalc.manageAction("*");
+        myCalc.manageAction("7");
+        assertEquals("14", myCalc.manageAction("="));
+    }
+    @Test
+    public void checkComputeDiv() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("8");
+        myCalc.manageAction("/");
+        myCalc.manageAction("2");
+        assertEquals("4", myCalc.manageAction("="));
+    }
+    @Test
+    public void checkComputeWithoutSecondOperand() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("8");
+        myCalc.manageAction("*");
+        assertEquals("64", myCalc.manageAction("="));
+    }
+    @Test
+    public void checkComputeSeveralOperationsWithoutEqual() throws Exception {
+        CalcUL myCalc = new CalcUL();
+        myCalc.manageAction("C");
+        myCalc.manageAction("8");
+        myCalc.manageAction("*");
+        myCalc.manageAction("2");
+        assertEquals("16", myCalc.manageAction("+"));
+    }
 }
