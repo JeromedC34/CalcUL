10debb501a30016a0fd7d57e424b7d8d1d119bac
Jerome <jdc@free.fr>
2016-10-06 16:02:54 +0200

    first_step (still UI logic in CalcUL with previous operation and operand managed)

--- app/src/main/java/com/jerome/calcul/CalcUL.java
+++ app/src/main/java/com/jerome/calcul/CalcUL.java
@@ -1,213 +1,85 @@
 package com.jerome.calcul;
 
-import android.media.AudioManager;
-import android.media.ToneGenerator;
-
-import java.util.Arrays;
-
 /**
  * Created by Human Booster on 04/10/2016.
  */
-public class CalcUL {
-    private static StringBuffer display;
-    private static StringBuffer operand1;
-    private static StringBuffer operand2;
-    private static StringBuffer operation;
-    private static boolean pending_operand;
-    private static StringBuffer pending_operation;
-    private final String defaultValue = "0";
+class CalcUL {
+    private double operand;
+    private String operation;
 
     public CalcUL() {
-        if (display == null) {
-            display = new StringBuffer();
-            operand1 = new StringBuffer();
-            operand2 = new StringBuffer();
-            operation = new StringBuffer();
-            pending_operand = false;
-            pending_operation = new StringBuffer();
-            inputClear();
-        }
+        clear();
     }
 
-    public String getDisplay() {
-        return display.toString();
+    public void clear() {
+        operand = 0;
+        operation = "";
     }
 
-    private void setDisplay(String value) {
-        display.delete(0, display.length());
-        display.append(value.replaceAll("\\.0*$", ""));
-    }
-
-    private void setDisplay(double value) {
-        setDisplay(String.valueOf(value).replaceAll("\\.0*$", ""));
-    }
-
-    public String manageAction(String myTag) {
-        if (myTag.equals("C")) {
-            inputClear();
-        } else if (Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0").contains(myTag)) {
-            inputDigit(myTag);
-        } else if (myTag.equals(".")) {
-            inputDecimalSeparator(myTag);
-        } else if (Arrays.asList("*", "-", "+", "/").contains(myTag)) {
-            inputOperation(myTag);
-        } else if (myTag.equals("=")) {
-            inputEqual();
-        }
-        return getDisplay();
-    }
-
-    private void inputClear() {
-        setDisplay(defaultValue);
-        setOperand1("");
-        setOperand2("");
-        setOperation("");
-        setPendingOperand(false);
-        setPendingOperation("");
-    }
-
-    private void inputDigit(String myTag) {
-        if (defaultValue.equals(display.toString())) {
-            display.delete(0, display.length());
-        }
-        changePendingOperand(myTag);
-    }
-
-    private void inputDecimalSeparator(String myTag) {
-        if (display.lastIndexOf(myTag) < 0) {
-            changePendingOperand(myTag);
-        }
-    }
-
-    private void changePendingOperand(String myTag) {
-        stockPendingOperation();
-        setOperand2("");
-        if (!pending_operand) {
-            if (myTag.equals(".")) {
-                setDisplay(defaultValue);
-            } else {
-                setDisplay("");
-            }
-        }
-        display.append(myTag);
-        setPendingOperand(true);
-    }
-
-    private void inputOperation(String myTag) {
-        if (!operand1.toString().equals("") && !operation.toString().equals("") && pending_operand) {
-            inputEqual();
-        }
-        setOperand1(display.toString());
-        setOperation("");
-        setPendingOperation(myTag);
-        setPendingOperand(false);
-    }
-
-    private double getOperand1() {
-        double current_operand1;
-        if (operand1.toString().equals("")) {
-            current_operand1 = getOperand2();
+    public void setOperand(double newOperand) {
+        if (!operation.equals("")) {
+            operand = setCompute(operand, operation, newOperand);
         } else {
-            current_operand1 = Double.valueOf(operand1.toString());
+            operand = newOperand;
         }
-        return current_operand1;
     }
 
-    private void setOperand1(String value) {
-        operand1.delete(0, operand1.length());
-        operand1.append(value);
+    public double setDivide() {
+        operation = "/";
+        return operand;
     }
 
-    private double getOperand2() {
-        double current_operand2;
-        if (operand2.toString().equals("")) {
-            current_operand2 = Double.valueOf(getDisplay());
-            setOperand2(String.valueOf(current_operand2));
+    public double setMultiple() {
+        operation = "*";
+        return operand;
+    }
+
+    public double setMinus() {
+        operation = "-";
+        return operand;
+    }
+
+    public double setPlus() {
+        operation = "+";
+        return operand;
+    }
+
+    public double setEqual() {
+        return operand;
+    }
+
+    private double setCompute(double operand1, String operation, double operand2) {
+        switch (operation) {
+            case "+":
+                return doPlus(operand1, operand2);
+            case "-":
+                return doMinus(operand1, operand2);
+            case "*":
+                return doMultiple(operand1, operand2);
+            case "/":
+                return doDivide(operand1, operand2);
+            default:
+                return this.operand;
+        }
+    }
+
+    private double doPlus(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+
+    private double doMinus(double operand1, double operand2) {
+        return operand1 - operand2;
+    }
+
+    private double doMultiple(double operand1, double operand2) {
+        return operand1 * operand2;
+    }
+
+    private double doDivide(double operand1, double operand2) {
+        if (operand2 == 0) {
+            throw new ArithmeticException("Division by zero!");
         } else {
-            current_operand2 = Double.valueOf(operand2.toString());
+            return operand1 / operand2;
         }
-        setOperand2(String.valueOf(current_operand2));
-        setPendingOperand(false);
-        return current_operand2;
-    }
-
-    private void setOperand2(String value) {
-        operand2.delete(0, operand2.length());
-        operand2.append(value);
-    }
-
-    private void checkPendingOperation() {
-        if (!pending_operation.toString().equals("")) {
-            setOperation(pending_operation.toString());
-            setPendingOperation("");
-        }
-    }
-
-    private void inputEqual() {
-        double current_operand1 = getOperand1();
-        double current_operand2 = getOperand2();
-        double current_result = Double.valueOf(defaultValue);
-        checkPendingOperation();
-        if (operation.toString().equals("/")) {
-            current_result = doDiv(current_operand1, current_operand2);
-        } else if (operation.toString().equals("*")) {
-            current_result = doMul(current_operand1, current_operand2);
-        } else if (operation.toString().equals("-")) {
-            current_result = doMin(current_operand1, current_operand2);
-        } else if (operation.toString().equals("+")) {
-            current_result = doAdd(current_operand1, current_operand2);
-        }
-        setDisplay(current_result);
-        setOperand1(getDisplay());
-    }
-
-    private double doDiv(double operand1, double operand2) {
-        double current_result;
-        if (operand2 != 0) {
-            current_result = operand1 / operand2;
-        } else {
-            inputClear();
-            current_result = Double.valueOf(defaultValue);
-            ToneGenerator toneG = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
-            toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
-        }
-        return current_result;
-    }
-
-    private double doMul(double operand1, double operand2) {
-        double current_result = operand1 * operand2;
-        return current_result;
-    }
-
-    private double doMin(double operand1, double operand2) {
-        double current_result = operand1 - operand2;
-        return current_result;
-    }
-
-    private double doAdd(double operand1, double operand2) {
-        double current_result = operand1 + operand2;
-        return current_result;
-    }
-
-    private void stockPendingOperation() {
-        if (!pending_operation.toString().equals("")) {
-            setOperation(pending_operation.toString());
-            setPendingOperation("");
-            setDisplay(defaultValue);
-        }
-    }
-
-    private void setPendingOperation(String value) {
-        pending_operation.delete(0, pending_operation.length());
-        pending_operation.append(value);
-    }
-
-    private void setPendingOperand(boolean value) {
-        pending_operand = value;
-    }
-
-    private void setOperation(String value) {
-        operation.delete(0, operation.length());
-        operation.append(value);
     }
 }
--- app/src/main/java/com/jerome/calcul/MainActivity.java
+++ app/src/main/java/com/jerome/calcul/MainActivity.java
@@ -7,18 +7,126 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected CalcUL calcUL = new CalcUL();
-    protected TextView textView;
+    private final static String defaultValue = "0";
+    private static CalcUL calcUL = new CalcUL();
+    private static String display = defaultValue;
+    private static String lastOperand = "";
+    private static String lastOperation = "";
+    private static boolean operandBeingInput = false;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text_result);
-        textView.setText(calcUL.getDisplay());
+        setDisplay(display);
     }
 
     public void manageAction(View view) {
-        textView.setText(calcUL.manageAction(view.getTag().toString()));
+        switch (view.getTag().toString()) {
+            case "C":
+                chooseClear();
+                break;
+            case "/":
+                chooseDivide();
+                break;
+            case "*":
+                chooseMultiple();
+                break;
+            case "-":
+                chooseMinus();
+                break;
+            case "+":
+                choosePlus();
+                break;
+            case "=":
+                chooseEqual();
+                break;
+            case ".":
+                chooseDecimalSeparator();
+                break;
+            default: // digits
+                chooseDigit(view.getTag().toString());
+                break;
+        }
+    }
+
+    private void setDisplay() {
+        textView.setText(display);
+    }
+
+    private void setDisplay(String newDisplay) {
+        display = newDisplay;
+        setDisplay();
+    }
+
+    private void setDisplay(double newDisplay) {
+        display = String.valueOf(newDisplay).replaceAll("\\.0*$", "");
+        setDisplay();
+    }
+
+    private void setOperand() {
+        if (operandBeingInput) {
+            lastOperand = textView.getText().toString();
+            calcUL.setOperand(Double.valueOf(lastOperand));
+            operandBeingInput = false;
+        }
+    }
+
+    private void chooseClear() {
+        calcUL.clear();
+        operandBeingInput = false;
+        setDisplay(defaultValue);
+    }
+
+    private void chooseDivide() {
+        setOperand();
+        setDisplay(calcUL.setDivide());
+    }
+
+    private void chooseMultiple() {
+        setOperand();
+        setDisplay(calcUL.setMultiple());
+    }
+
+    private void chooseMinus() {
+        setOperand();
+        setDisplay(calcUL.setMinus());
+    }
+
+    private void choosePlus() {
+        setOperand();
+        setDisplay(calcUL.setPlus());
+    }
+
+    private void chooseEqual() {
+        // while equal asked without new operand input the last operand (and operation) is re-used
+        if (!operandBeingInput && !lastOperand.equals("")) {
+            calcUL.setOperand(Double.valueOf(lastOperand));
+        } else {
+            setOperand();
+        }
+        setDisplay(calcUL.setEqual());
+    }
+
+    private void chooseDecimalSeparator() {
+        if (display.lastIndexOf(".") < 0) {
+            if (!operandBeingInput) {
+                operandBeingInput = true;
+                setDisplay(defaultValue + ".");
+            } else {
+                setDisplay(display + ".");
+            }
+        }
+    }
+
+    private void chooseDigit(String digit) {
+        if (!operandBeingInput) {
+            operandBeingInput = true;
+            setDisplay(digit);
+        } else {
+            setDisplay(display + digit);
+        }
     }
 }
