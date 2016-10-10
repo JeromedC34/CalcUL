8349e75768631318a8ff78cc95f2c8b7ae11e7e1
Jerome <jdc@free.fr>
2016-10-10 15:23:54 +0200

    fragments (normal+scientific) added, code not changed (in MainActivity)

---
+++ app/src/main/java/com/jerome/calcul/ScientificFragment.java
@@ -0,0 +1,21 @@
+package com.jerome.calcul;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ScientificFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_scientific, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    public void manageAction(View view) {
+    }
+}
