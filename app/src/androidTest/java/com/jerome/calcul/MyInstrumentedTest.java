168678481b862695d92a9a35225697142476cc9e
Jerome <jdc@free.fr>
2016-10-12 14:15:05 +0200

    rename test files CheckUI -> MyInstrumentedTest + ExampleUnitTest -> MyUnitTest

--- app/src/androidTest/java/com/jerome/calcul/MyInstrumentedTest.java
+++ app/src/androidTest/java/com/jerome/calcul/MyInstrumentedTest.java
@@ -21,7 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class CheckUI {
+public class MyInstrumentedTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
